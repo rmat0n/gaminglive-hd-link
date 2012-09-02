f10c243043f188a97a65b6d2f15c054b54509791
rmat0n <romain.maton@gmail.com>
2012-09-02 14:18:41 +0200

    Create chrome project.

---
+++ README.md
@@ -0,0 +1,4 @@
+Gaming-Live HD link
+===================
+
+Extension Chrome permettant d'ajouter un lien de téléchargement HD sur les vidéos [Gaming Live](http://www.jeuxvideo.com/gaming-live/) de [Jeux-Video.com](http://www.jeuxvideo.com/).
---
+++ chrome/background.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2012 Romain MATON. All rights reserved.
+chrome.tabs.onUpdated.addListener(function (tab_id, data, tab) {
+    if (tab.url.indexOf("http://www.jeuxvideo.com/gaming-live/") != -1) {
+        chrome.pageAction.show(tab_id);
+    }
+});
