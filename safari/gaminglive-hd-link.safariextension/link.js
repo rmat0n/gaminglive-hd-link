080f86ea8e517cbb21c7f392b6e5982534aeda34
rmat0n <romain.maton@gmail.com>
2014-11-26 22:07:24 +0100

    DRY update structure.

--- safari/gaminglive-hd-link.safariextension/link.js
+++ safari/gaminglive-hd-link.safariextension/link.js
@@ -1,12 +1,1 @@
-// Copyright (c) 2012 Romain MATON. All rights reserved.
-// Extension permettant d'ajouter un lien de téléchargement HD sur les vidéos Gaming Live de Jeux-Video.com
-$(document).ready(function() {
-    var $mp4 = $("#mp4");
-    var $mp4HD = $mp4.clone();
-    $mp4HD.attr("id", "mp4HD")
-          .find("a").attr("href", $mp4.find("a").attr("href").replace("-low", "-high"))
-                    .css({"display": "table-cell", "vertical-align": "middle", "text-align": "center"})
-          .find("span").text("HD")
-                       .css({"position": "relative", "left": "0px", "color": "white"});
-    $("#barre_fonctions").append($mp4HD);
-});
+../../lib/link.js
