8408e37789c801e1fdf6f3d2c1cd44b467a59105
rmat0n <romain.maton@gmail.com>
2014-11-27 22:05:22 +0100

    DRY add gulp to copy js and icons on change in all plugins.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+
+gulp.task('lint', function() {
+  return gulp.src('lib/link.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('libs/*.js')
+    .pipe(gulp.dest('chrome'))
+    .pipe(gulp.dest('safari/gaminglive-hd-link.safariextension'));
+});
+
+gulp.task('icons', function() {
+  return gulp.src('icons/*.png')
+    .pipe(gulp.dest('chrome'))
+    .pipe(rename(function (path) {
+      path.basename = path.basename.substring(0, 4) + "-" + path.basename.substring(4);
+    }))
+    .pipe(gulp.dest('safari/gaminglive-hd-link.safariextension'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('js/*.js', ['lint', 'scripts']);
+  gulp.watch('icons/*.png', ['icons']);
+});
+
+gulp.task('default', ['lint', 'scripts', 'icons', 'watch']);
