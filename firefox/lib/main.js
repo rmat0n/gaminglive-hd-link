a4bcc43ca6d43034005f698eef8c8955ce62d824
rmat0n <romain.maton@gmail.com>
2015-02-15 12:13:42 +0100

    Update with new website.

--- firefox/lib/main.js
+++ firefox/lib/main.js
@@ -2,6 +2,6 @@
 // Extension permettant d'ajouter un lien de téléchargement HD sur les vidéos Gaming Live de Jeux-Video.com
 var data = require("sdk/self").data;
 require("sdk/page-mod").PageMod({
-  include: 'http://www.jeuxvideo.com/gaming-live/*',
+  include: 'http://www.jeuxvideo.com/videos/gaming-live/*',
   contentScriptFile: [ data.url("jquery-1.7.2.js"), data.url("link.js") ]
 });
--- libs/link.js
+++ libs/link.js
@@ -2,14 +2,17 @@
 // Extension permettant d'ajouter un lien de téléchargement HD sur les vidéos Gaming Live de Jeux-Video.com
 $(document).ready(function () {
   'use strict';
-  var $mp4 = $("#mp4");
-  if ($mp4.length) {
-    var $mp4HD = $mp4.clone();
-    $mp4HD.attr("id", "mp4HD")
-      .find("a").attr("href", $mp4.find("a").attr("href").replace("-low", "-high"))
-      .css({"display": "table-cell", "vertical-align": "middle", "text-align": "center"})
-      .find("span").text("HD")
-      .css({"position": "relative", "left": "0px", "color": "white"});
-    $("#barre_fonctions").append($mp4HD);
+  var $videoUrl = $('.row .header-video meta[itemprop="contentUrl"]');
+  console.log('GAMING LIVE: '+ $videoUrl.attr('content'));
+  if ($videoUrl.length) {
+    $('.row .player-contenu').closest('.row').after(
+      '<div class="row" style="margin-bottom: 10px;">' +
+        '<div class="col-md-8">' +
+          '<div style="text-align: right; margin-right: 5px;">' +
+            '<a href="' + $videoUrl.attr('content') + '" download>T&eacute;l&eacute;charger la vid&eacute;o en HD</a>' +
+          '</div>' +
+        '</div>' +
+      '</div>'
+    );
   }
 });
